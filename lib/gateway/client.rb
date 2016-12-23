849ca234d464b66001f42e7a0f77b87924d307d9
denis <avenger_den@mail.ru>
2016-12-23 18:33:48 +0300

    check new url in gateway

--- lib/gateway/client.rb
+++ lib/gateway/client.rb
@@ -43,20 +43,20 @@ module Gateway
     #
     def response
       # Rails.logger.debug('gateway request: ' + YAML::dump(request))
-      query_params = {r: config_data.route}
-      # @url += config_data.route
+      # query_params = {r: config_data.route}
+      @url += config_data.route
       case config_data.http_method
         when METHOD_GET
-          query_params.merge! request.attributes
-          # query_params = request.attributes
+          # query_params.merge! request.attributes
+          query_params = request.attributes
           response = RestClient.get(url(query_params))
         when METHOD_POST
           if config_data.content_type == 'json'
-            response = RestClient.post(url(query_params), request.attributes.to_json, content_type: :json, Authorization: @token)
-            # response = RestClient.post(url, request.attributes.to_json, content_type: :json, Authorization: @token)
+            # response = RestClient.post(url(query_params), request.attributes.to_json, content_type: :json, Authorization: @token)
+            response = RestClient.post(url, request.attributes.to_json, content_type: :json, Authorization: @token)
           else
-            response = RestClient.post(url(query_params), request.attributes, Authorization: @token)
-            # response = RestClient.post(url, request.attributes, Authorization: @token)
+            # response = RestClient.post(url(query_params), request.attributes, Authorization: @token)
+            response = RestClient.post(url, request.attributes, Authorization: @token)
           end
         else
           raise Gateway::ArgumentError.new("http method #{config_data.http_method} is not supported")
@@ -75,8 +75,8 @@ module Gateway
     #
     # @return [String] url with query params
     #
-    # def url(query_params = {})
-    def url(query_params)
+    def url(query_params = {})
+    # def url(query_params)
       url = @url
       url += "?#{URI.encode_www_form(query_params)}" unless query_params.empty?
       url
